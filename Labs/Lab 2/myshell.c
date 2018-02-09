b09f772d737f425cd4600113ab71e240cb0bc983
AshwinK97 <ashwin.kamalakannan@uoit.net>
2018-02-09 12:15:28 -0500

    string tokenizing for lab 2

--- Labs/Lab 2/myshell.c
+++ Labs/Lab 2/myshell.c
@@ -21,38 +21,42 @@
 
 // Define functions declared in myshell.h here
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+
     // Input buffer and and commands
     char buffer[BUFFER_LEN] = { 0 };
     char command[BUFFER_LEN] = { 0 };
     char arg[BUFFER_LEN] = { 0 };
+	char* token;
 
     // Parse the commands provided using argc and argv
 
     // Perform an infinite loop getting command input from users
-    while (fgets(buffer, BUFFER_LEN, stdin) != NULL)
-    {
-        // Perform string tokenization to get the command and argument
+    while (fgets(buffer, BUFFER_LEN, stdin) != NULL) {
+        
+		// Perform string tokenization to get the command and argument
+		token = strtok(buffer, "\n");
+		token = strtok(buffer, " ");
+		strcpy(command, token);
 
         // Check the command and execute the operations for each command
         // cd command -- change the current directory
-        if (strcmp(command, "cd") == 0)
-        {
-            // your code here
+        if (strcmp(command, "cd") == 0) {
+			token = strtok(NULL, " ");
+			strcpy(arg, token);
+			printf("you changed directory to %s\n", arg);
         }
 
         // other commands here...
         
         // quit command -- exit the shell
-        else if (strcmp(command, "quit") == 0)
-        {
+		else if (strcmp(command, "exit") == 0) {
+			printf("Exiting shell\n");
             return EXIT_SUCCESS;
         }
 
         // Unsupported command
-        else
-        {
+        else {
             fputs("Unsupported command, use help to display the manual\n", stderr);
         }
     }
