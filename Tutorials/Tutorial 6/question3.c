fb280f1fc803b47fd8eec83e234979ac3e2e4a0e
AshwinK97 <ashwin.kamalakannan@uoit.net>
2018-03-05 20:59:24 -0500

    completed question 3 and 4

--- Tutorials/Tutorial 6/question3.c
+++ Tutorials/Tutorial 6/question3.c
@@ -1,36 +1,79 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <pthread.h>
+#include <semaphore.h>
 
-#define FILE1 "child1.txt"
-#define FILE2 "child2.txt"
+#define COUNT 5
+
+// semaphore
+sem_t semaphore;
+
+// global variables
+int moving_sum[5] = {0, 0, 0, 0, 0};
+
+int factorial(int x) {
+	if (x==1) return x;
+	return x * factorial(x-1);
+}
+
+void *task(void *arg) {
+	int *arr = (int *)arg; // convert pointer arg to int array
+	int i = arr[0], input = arr[1]; // get i and input from array
+	int fact = factorial(input); // calculate the factorial of input
+	
+	sem_wait(&semaphore); // lock semaphore
+	
+	if (i > 0 && moving_sum[i-1] > 0)
+		moving_sum[i] = fact + moving_sum[i-1];
+	else
+		moving_sum[i] = fact;
+
+	sem_post(&semaphore); // unlock semaphore
+	pthread_exit(0);
+}
 
 int main() {
-	pid_t pid1, pid2;
+	pthread_t threads[COUNT];
 
-	if ((pid1 = fork()) == 0) { // child 1
-		sleep(1);
-		printf("I am child 1\n");
-	} 
-	else {
-		if ((pid2 = fork()) == 0) { // child 2
-			sleep(1);
-			printf("I am child 2\n");
-		} 
-		else { // parent
-			FILE *fp1 = fopen(FILE1, "w");
-			FILE *fp2 = fopen(FILE2, "w");
-			fprintf(fp1, "child 1");
-			fprintf(fp2, "child 2");
-			fclose(fp1);
-			fclose(fp2);
-			printf("I am the parent\n");
-			
-			waitpid(pid1, NULL, 0);
-			waitpid(pid2, NULL, 0);
+	// initialize semaphore for single access
+	if (sem_init(&semaphore, 0, 1)) {
+		printf("could not initialize semaphore\n");
+		return -1;
+	}
+	
+	// get inputs and create threads
+	int i, input, arg[2] = {0, 0};
+	for (i=0; i<COUNT; i++) {
+		
+		// get input
+		printf("Enter number %d: ", i);
+		scanf("%d", &input);
+		
+		// set arguments
+		arg[0] = i;
+		arg[1] = input;
+		
+		// create thread and pass arguments
+		if (pthread_create(&threads[i], NULL, &task, &arg)) {
+			printf("failed to create thread %d\n", i);
+			return -1;
+		}
+	}
+
+	// wait for threads to exit before terminating
+	for (i=0; i<COUNT; i++) {
+		if (pthread_join(threads[i], NULL)) {
+			printf("failed to join thread %d\n", i);
+			return -1;
 		}
 	}
 	
+	// free resources
+	sem_destroy(&semaphore);
+	
+	// print array
+	for (i=0; i<COUNT; i++)
+		printf("%d ", moving_sum[i]);
+	printf("\n");
 }
