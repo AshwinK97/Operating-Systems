e323f1233d36febf3b102713ca2a4f026300f633
AshwinK97 <ashwin.kamalakannan@uoit.net>
2018-03-26 14:35:44 -0400

    changed to hard tabs

--- Tutorials/Tutorial 9/question2.c
+++ Tutorials/Tutorial 9/question2.c
@@ -13,27 +13,27 @@ double calculate(int num)
 
 int main (int argc, char* argv[])
 {
-    double x = 0; // Watch out for race conditions!
+		double x = 0; // Watch out for race conditions!
 		int n = 100000;
 		double y[n];
 		double dx = 1 / (n+1);
-    int thread_num, nthreads = atoi(argv[1]);
-    #ifdef _OPENMP
-    omp_set_num_threads(nthreads);
-    #endif
-    printf("Testing OpenMP, you should see each thread print...\n");
-    #pragma omp parallel for private(x)
-    for (int i = 0; i < n; ++i)
-    {
-        x = i * dx;
+		int thread_num, nthreads = atoi(argv[1]);
+		#ifdef _OPENMP
+		omp_set_num_threads(nthreads);
+		#endif
+		printf("Testing OpenMP, you should see each thread print...\n");
+		#pragma omp parallel for private(x)
+		for (int i = 0; i < n; ++i)
+		{
+				x = i * dx;
 				printf("\nx: %f, i: %d, dx: %f", x, i, dx);
 				y[i] = calculate(x);
-        #pragma omp critical
-        {
-          #ifdef _OPENMP
-          thread_num = omp_get_thread_num();
-          //printf("This thread = %d, num = %d, expensive = %f\n", thread_num, i, y[i]);
-          #endif
-        }
-    }
+				#pragma omp critical
+				{
+					#ifdef _OPENMP
+					thread_num = omp_get_thread_num();
+					//printf("This thread = %d, num = %d, expensive = %f\n", thread_num, i, y[i]);
+					#endif
+				}
+		}
 }
