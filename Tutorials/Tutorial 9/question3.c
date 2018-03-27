fda1a92e12e1abd3d29e7c5f5766a197729f6b4b
AshwinK97 <ashwin.kamalakannan@uoit.net>
2018-03-27 00:37:55 -0400

    added pdf, question 3 done but with bugs

--- Tutorials/Tutorial 9/question3.c
+++ Tutorials/Tutorial 9/question3.c
@@ -2,26 +2,34 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <math.h>
 #include <omp.h>
 
-#define N 100000
+#define N 100000000
 
 int main () {
+	clock_t start;
 	srand(time(NULL));
 	int x[N];
-
+	double snorm = 0, pnorm = 0;
 	long int i;
-	for (i=0; i<N; i++) {
-		x[i] = rand();
-		printf("%d, ", x[i]);
-	}
-	printf("\n");
 
-	//#pragma omp parallel for private(x, tid) num_threads(nthreads)
-	//for (long int i = 0; i < n; i++) {
-	//	x = i * dx;
-	//	tid = omp_get_thread_num();
-	//	printf("thread %d -> x: %f, i: %ld, dx: %f\n", tid, x, i, dx);
-	//	y[i] = exp(x) * cos(x) * sin(x) * sqrt(5 * x + 6.0);
-	//}
+	// generate random numbers
+	for (i=0; i<N; i++)
+		x[i] = rand();
+
+	// serial loop
+	start = clock();
+	for (i=0; i<N; i++)
+		snorm += fabs(x[i]);
+	printf("Serial loop elapsed time: %.6f\n", (double)(clock() - start)/CLOCKS_PER_SEC);	
+	printf("Serial norm = %f\n", snorm);
+
+	// parallel loop
+	start = clock();
+	#pragma omp parallel for num_threads(10) reduction(+:pnorm)
+	for (i=0; i<N; i++)
+		pnorm += fabs(x[i]);
+	printf("Parallel loop elapsed time: %6f\n", (double)(clock() - start)/CLOCKS_PER_SEC);
+	printf("Parallel norm = %f\n", pnorm);
 }
