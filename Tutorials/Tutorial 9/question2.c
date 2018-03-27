53cf1bed4e6259f7a299b77fd074ded3f6483922
AshwinK97 <ashwin.kamalakannan@uoit.net>
2018-03-26 22:25:03 -0400

    question 2 working

--- Tutorials/Tutorial 9/question2.c
+++ Tutorials/Tutorial 9/question2.c
@@ -5,35 +5,19 @@
 #include <math.h>
 #include <omp.h>
 
-double calculate(int num)
-{
-		double result = (exp(num) * cos(num) * sin(num) * sqrt(5 * num + 6.0));
-		return result;
-}
-
-int main (int argc, char* argv[])
-{
-		double x = 0; // Watch out for race conditions!
-		int n = 100000;
-		double y[n];
-		double dx = 1 / (n+1);
-		int thread_num, nthreads = atoi(argv[1]);
-		#ifdef _OPENMP
-		omp_set_num_threads(nthreads);
-		#endif
-		printf("Testing OpenMP, you should see each thread print...\n");
-		#pragma omp parallel for private(x)
-		for (int i = 0; i < n; ++i)
-		{
-				x = i * dx;
-				printf("\nx: %f, i: %d, dx: %f", x, i, dx);
-				y[i] = calculate(x);
-				#pragma omp critical
-				{
-					#ifdef _OPENMP
-					thread_num = omp_get_thread_num();
-					//printf("This thread = %d, num = %d, expensive = %f\n", thread_num, i, y[i]);
-					#endif
-				}
-		}
+int main (int argc, char* argv[]) {
+	double x = 0;
+	long int n = 100000000;
+	double y[n];
+	double dx = 1 / ((double)n+1);
+	int tid;
+	int nthreads = atoi(argv[1]);
+	
+	#pragma omp parallel for private(x, tid) num_threads(nthreads)
+	for (long int i = 0; i < n; i++) {
+		x = i * dx;
+		tid = omp_get_thread_num();
+		printf("thread %d -> x: %f, i: %ld, dx: %f\n", tid, x, i, dx);
+		y[i] = exp(x) * cos(x) * sin(x) * sqrt(5 * x + 6.0);
+	}
 }
